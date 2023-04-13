b2ff1c6777aa9f01480b4646df467552c1d06fda
Lotosney <sebastiandwojcik@gmail.com>
2023-04-13 19:14:38 +0200

    Add tobacco statistics subpage

---
+++ tobacco/sales-chart.php
@@ -0,0 +1,109 @@
+<?php
+
+$query = $pdo->prepare("SELECT  SUM(`product_amount`) AS `summed_amount`, SUM(`product_amount`*`product_retail_price`) 
+AS `retailed`, `sell_date`, SUM(`product_amount` * `final_price`) AS `revenue` FROM `products_sold` 
+ WHERE `product_type` = 'Artykuły Tytoniowe' AND `store_email` =  '" . $email . "' GROUP BY `sell_date`  ");
+
+$query->execute();
+while ($row = $query->fetch()) {
+    $dateArray[]= $row["sell_date"];
+    $retailArray[]=$row["retailed"];
+    $priceArray[]= $row["revenue"];
+    $volumeArray[]= $row["summed_amount"];
+    
+}
+
+?>
+<script>
+    const dateArrayJS = <?php echo json_encode($dateArray); ?>; 
+    const volumeArrayJS = <?php echo json_encode($volumeArray); ?>;
+    
+    const dateChartJS = dateArrayJS.map((day,index)=>{
+        let dayjs =new Date(day);
+        return dayjs.setHours(0, 0,0,0)
+    })
+
+
+    const data = {
+      labels: dateChartJS,
+      datasets: [{
+        label: 'Kwota bez VAT',
+        data: <?php echo json_encode($priceArray); ?>,
+        backgroundColor: [
+          'green',
+        ],
+        borderColor: [
+          '#4CAF50',
+        ],
+        borderWidth: 1
+      },{
+        label: 'Kwota z VAT',
+        data: <?php echo json_encode($retailArray); ?>,
+        backgroundColor: [
+          'blue',
+        ],
+        borderColor: [
+          'blue',
+        ],
+        borderWidth: 1
+      },{
+       
+        label: 'Ilość sprzedanego produktu',
+        data: <?php echo json_encode($volumeArray); ?>,
+        backgroundColor: [
+          'red',
+        ],
+        borderColor: [
+          'red',
+        ],
+        borderWidth: 1
+      },
+
+    ]
+    };
+  
+    const config = {
+      type: 'line',
+      data,
+      options: {
+        responsive: true, 
+      maintainAspectRatio: false,
+        scales: {
+            x:{
+                min:'2023-04-01',
+                max:'2023-04-31',
+                type:'time',
+                time:{
+                    unit:'day'
+                }
+            },
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+
+    }
+
+
+
+
+    const salesChart = new Chart(
+      document.getElementById('salesChart'),
+      config
+    );
+
+
+
+    function startDateFilter(date){
+        const startDate = new Date(date.value)
+        salesChart.config.options.scales.x.min=startDate.setHours(0,0,0,0)
+        salesChart.update();
+    }
+    function endDateFilter(date){
+        const endDate = new Date(date.value)
+
+        salesChart.config.options.scales.x.max=endDate.setHours(0,0,0,0)
+        salesChart.update();
+    }
+</script>
--- tobacco/statistics.php
+++ tobacco/statistics.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>StoreManagerHelp - Statystyki Kawa i Herbata</title>
+  <title>StoreManagerHelp - Statystyki Artykuły Tytoniowe</title>
   <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js/dist/chart.umd.min.js"></script>
 
   <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-date-fns/dist/chartjs-adapter-date-fns.bundle.min.js"></script>
@@ -43,7 +43,7 @@ if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
           <a href="../main/main-statistics.php">Główne Statystyki</a>
           <a href="../dairy/statistics.php">Nabiał</a>
           <a href="../alcohol/statistics.php">Alkohol</a>
-          <a href="../tobacco/statistics.php">Wyroby Tytoniowe</a>
+          <a class="active"  href="statistics.php">Wyroby Tytoniowe</a>
           <a href="../snacks/statistics.php">Przekąski</a>
           <a href="../sweets/statistics.php">Słodycze</a>
           <a href="../bakery/statistics.php">Pieczywo</a>
@@ -56,7 +56,7 @@ if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
           <a href="../cosmetics/statistics.php">Kosmetyki</a>
           <a href="../cleaning/statistics.php">Środki Czystości</a>
           <a href="../beverages/statistics.php">Soki i Napoje</a>
-          <a class="active"  href="statistics.php">Kawa i Herbata</a>
+          <a href="../tea/statistics.php">Kawa i Herbata</a>
         </div>
   </div> 
   <li style="float:right"><a class="logout" href="../logout.php"><h3>Wyloguj się</h3></a></li>
@@ -88,7 +88,7 @@ if (!isset($_SESSION['email']) || empty($_SESSION['email'])) {
      
       </div>
       <form>
-        <h1> Kawa i Herbata</h1>
+        <h1> Artykuły Tytoniowe</h1>
         <div class="tab">
         <table class="table">
           <thead>
